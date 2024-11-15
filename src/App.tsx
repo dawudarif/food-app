95051f36f63499a546167adf6baf1d6a5f474b9d
Dawood Arif <dawuddarif@gmail.com>
2024-11-15 22:27:03 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from "react-router";
+import { Routes } from "react-router-dom";
+import "./globals.scss";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+
+const routes = [
+  { path: "/", element: <Home /> },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {routes.map((item, index) => {
+        return <Route key={index} path={item.path} element={item.element} />;
+      })}
+    </Routes>
   );
 }
 
