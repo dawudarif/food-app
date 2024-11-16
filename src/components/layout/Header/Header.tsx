4c9c7f3654b6dd7797b4a04ec6e3b31ce879b32f
Dawood Arif <dawuddarif@gmail.com>
2024-11-16 16:19:53 +0500

    feat: updated header for lg screens

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,7 +1,11 @@
-import { Phone, Mail, Facebook, Twitter, Instagram } from "lucide-react";
+import { Phone, Mail, Facebook, Twitter, Instagram, Menu } from "lucide-react";
+import { Link, useLocation } from "react-router-dom";
+import { routes } from "../../../App";
 import styles from "./Header.module.scss";
 
 export default function Header() {
+  const { pathname } = useLocation();
+
   return (
     <div className={styles.header}>
       <div className={styles.thinHeader}>
@@ -28,6 +32,39 @@ export default function Header() {
             <div className={styles.socialIcon}>
               <Instagram size={20} color="white" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.navbar}>
+        <div className="container">
+          <div className={styles.iconContainer}>
+            <img src="/favicon.svg" alt="logo" />
+            <p>Bistro Bliss</p>
+          </div>
+
+          <div className={styles.navContainer}>
+            <div className={styles.navLinks}>
+              {routes.map((item, index) => {
+                console.log(pathname, item.path);
+
+                return (
+                  <Link
+                    to={item.path}
+                    key={index}
+                    className={
+                      pathname === item.path ? styles.activeLink : undefined
+                    }
+                  >
+                    {item.name}
+                  </Link>
+                );
+              })}
+            </div>
+
+            <Menu color="#2c2f24" size={30} className={styles.menuIcon} />
+
+            <button className={styles.navBtn}>Book A Table</button>
           </div>
         </div>
       </div>
