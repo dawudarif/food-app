25393b19c81c900d935b6c0210525312a7887767
Dawood Arif <dawuddarif@gmail.com>
2024-11-17 21:07:16 +0500

    feat: added section OurServices to homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import HomeHero from "../../sections/HomeHero/HomeHero";
 import ImageWithTextAndDescription from "../../sections/ImageWithTextAndDescription/ImageWithTextAndDescription";
 import MenuLinks from "../../sections/MenuLinks/MenuLinks";
+import OurServices from "../../sections/OurServices/OurServices";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HomeHero />
       <MenuLinks />
       <ImageWithTextAndDescription />
+      <OurServices />
     </>
   );
 }
