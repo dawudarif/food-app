9b06ee09293d89d0c817261be89d3f827a4fccc8
Dawood Arif <dawuddarif@gmail.com>
2024-11-28 23:08:05 +0500

    refactor: removed RTL provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n/i18next";
-import RTLProvider from "./i18n/RTLProvider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,9 +13,7 @@ root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <BrowserRouter>
-        <RTLProvider>
-          <App />
-        </RTLProvider>
+        <App />
       </BrowserRouter>
     </I18nextProvider>
   </React.StrictMode>
