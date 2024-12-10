1369d828822202907370c04de1727672c6d440cf
Dawood Arif <dawuddarif@gmail.com>
2024-12-10 22:50:19 +0500

    chore: updated header, footer

---
+++ src/sections/OverlaySection/OverlaySection.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import styles from "./OverlaySection.module.scss";
+import uiIcon from "../../assets/ui.svg";
+import cuisineIcon from "../../assets/cuisine.svg";
+import timerIcon from "../../assets/timer.svg";
+import clsx from "clsx";
+
+const icons = [uiIcon, cuisineIcon, timerIcon];
+
+export default function OverlaySection() {
+  const { t } = useTranslation();
+
+  const features: Features[] = t("overlayFeatures", {
+    returnObjects: true,
+  });
+
+  return (
+    <section data-testid="overlay-section">
+      <div className={styles.overlay}>
+        <h1>Feel the authentic & original taste from us</h1>
+      </div>
+
+      <div className={clsx("container", styles.features)}>
+        {features?.map((item, i) => (
+          <div key={i}>
+            <img src={icons[i]} alt={"icon" + i} />
+            <span>
+              <h4>{item.title}</h4>
+              <h5>{item.description}</h5>
+            </span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
