0f7269d052dd933dc6347f0564d9b82a2cbf0219
Dawood Arif <dawuddarif@gmail.com>
2024-12-11 22:33:58 +0500

    feat: added translations for OurServices

--- src/sections/OurServices/OurServices.tsx
+++ src/sections/OurServices/OurServices.tsx
@@ -1,41 +1,39 @@
+import { useTranslation } from "react-i18next";
 import styles from "./OurServices.module.scss";
 
-const services = [
-  {
-    title: "Catering",
-    description:
-      "Delicious, customized menus crafted with fresh ingredients for any occasion.",
-    imageUrl:
-      "https://res.cloudinary.com/dowc89eyy/image/upload/v1731858091/food-app/yvbjrnqpeo4myju8kdvi.jpg",
-  },
-  {
-    title: "Birthdays",
-    description:
-      "Exciting themed parties with fun activities, décor, and custom treats.",
-    imageUrl:
-      "https://res.cloudinary.com/dowc89eyy/image/upload/v1731858158/food-app/xzfcfmr8kcgnwknfsbso.jpg",
-  },
-  {
-    title: "Weddings",
-    description:
-      "Comprehensive wedding planning to make your special day unforgettable.",
-    imageUrl:
-      "https://res.cloudinary.com/dowc89eyy/image/upload/v1731858098/food-app/fcyfdvp07r3vtx2vjvlr.jpg",
-  },
-  {
-    title: "Events",
-    description:
-      "Professional event management for corporate, social, and casual gatherings.",
-    imageUrl:
-      "https://res.cloudinary.com/dowc89eyy/image/upload/v1731858102/food-app/mytnhgxe0os1ovtouvtu.jpg",
-  },
-];
-
 export default function OurServices() {
+  const { t } = useTranslation();
+
+  const services = [
+    {
+      title: t("ourServices.catering.title"),
+      description: t("ourServices.catering.description"),
+      imageUrl:
+        "https://res.cloudinary.com/dowc89eyy/image/upload/v1731858091/food-app/yvbjrnqpeo4myju8kdvi.jpg",
+    },
+    {
+      title: t("ourServices.birthdays.title"),
+      description: t("ourServices.birthdays.description"),
+      imageUrl:
+        "https://res.cloudinary.com/dowc89eyy/image/upload/v1731858158/food-app/xzfcfmr8kcgnwknfsbso.jpg",
+    },
+    {
+      title: t("ourServices.weddings.title"),
+      description: t("ourServices.weddings.description"),
+      imageUrl:
+        "https://res.cloudinary.com/dowc89eyy/image/upload/v1731858098/food-app/fcyfdvp07r3vtx2vjvlr.jpg",
+    },
+    {
+      title: t("ourServices.events.title"),
+      description: t("ourServices.events.description"),
+      imageUrl:
+        "https://res.cloudinary.com/dowc89eyy/image/upload/v1731858102/food-app/mytnhgxe0os1ovtouvtu.jpg",
+    },
+  ];
   return (
     <section data-testid="our-services" className="container">
       <div className={styles.mainContainer}>
-        <h1>We also offer unique services for your events</h1>
+        <h1>{t("ourServices.heading")}</h1>
 
         <div className={styles.cardsParent}>
           {services.map((item, index) => {
