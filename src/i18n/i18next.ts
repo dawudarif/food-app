21987721d2b4fc75833071cb42845df8c6151976
Dawood Arif <dawuddarif@gmail.com>
2024-12-16 22:06:22 +0500

    refactor: utils, routes translations

--- src/i18n/i18next.ts
+++ src/i18n/i18next.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
-import { getTranslationLocales } from "../lib/getTranslationLocales";
+import { getTranslationLocales } from "../utils/getTranslationLocales";
 
 const labels = getTranslationLocales({ type: "label" }) as string[];
 
