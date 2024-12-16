b8b9cfc7d071f8c0f296c210cb6e509c91aeb09c
Dawood Arif <dawuddarif@gmail.com>
2024-12-16 22:46:38 +0500

    feat: added translations for ImageWithTextAndDescription

--- src/sections/ImageWithTextAndDescription/ImageWithTextAndDescription.tsx
+++ src/sections/ImageWithTextAndDescription/ImageWithTextAndDescription.tsx
@@ -2,21 +2,8 @@ import styles from "./ImageWithTextAndDescription.module.scss";
 import locationMarkerIcon from "../../assets/location-marker.svg";
 import phoneIcon from "../../assets/phone.svg";
 import mailIcon from "../../assets/mail.svg";
-
-const data = [
-  {
-    icon: phoneIcon,
-    content: "(414) 857 - 0107",
-  },
-  {
-    icon: mailIcon,
-    content: "contact@bistrobliss.com",
-  },
-  {
-    icon: locationMarkerIcon,
-    content: "837 W. Marshall Lane Marshalltown, IA 50158, Los Angeles",
-  },
-];
+import { transformNumericals } from "../../utils/transformNumericals";
+import { useTranslation } from "react-i18next";
 
 export default function ImageWithTextAndDescription({
   imageUrl,
@@ -25,6 +12,25 @@ export default function ImageWithTextAndDescription({
   imageUrl: string;
   showButton?: boolean;
 }) {
+  const { t } = useTranslation();
+
+  const data = [
+    {
+      icon: phoneIcon,
+      content: transformNumericals(
+        t("imageWithTextAndDescription.contactInfo.phone")
+      ),
+    },
+    {
+      icon: mailIcon,
+      content: t("imageWithTextAndDescription.contactInfo.email"),
+    },
+    {
+      icon: locationMarkerIcon,
+      content: t("imageWithTextAndDescription.contactInfo.location"),
+    },
+  ];
+
   return (
     <section
       data-testid="image-with-text-and-description"
@@ -39,7 +45,7 @@ export default function ImageWithTextAndDescription({
             }}
           >
             <div className={styles.imageContent}>
-              <h1>Come and Visit Us</h1>
+              <h1>{t("imageWithTextAndDescription.contactInfoHeading")}</h1>
               <div className={styles.contentParent}>
                 {data.map((item, index) => (
                   <div key={index} className={styles.iconContent}>
@@ -51,23 +57,17 @@ export default function ImageWithTextAndDescription({
             </div>
           </div>
           <div className={styles.contentContainer}>
-            <h1>We provide healthy food for your family.</h1>
+            <h1>{t("imageWithTextAndDescription.heading")}</h1>
             <div>
               <strong>
-                Our story began with a vision to create a unique dining
-                experience that merges fine dining, exceptional service, and a
-                vibrant ambiance. Rooted in city's rich culinary culture, we aim
-                to honor our local roots while infusing a global palate.
+                {t("imageWithTextAndDescription.boldDescription")}
               </strong>
-              <p>
-                At place, we believe that dining is not just about food, but
-                also about the overall experience. Our staff, renowned for their
-                warmth and dedication, strives to make every visit an
-                unforgettable event.
-              </p>
+              <p>{t("imageWithTextAndDescription.description")}</p>
             </div>
 
-            {showButton && <button>More About Us</button>}
+            {showButton && (
+              <button>{t("imageWithTextAndDescription.button")}</button>
+            )}
           </div>
         </div>
       </div>
