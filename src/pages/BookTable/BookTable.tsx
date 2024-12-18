168cc6ce3f94ad11fd35969d79ed5c11734e1430
Dawood Arif <dawuddarif@gmail.com>
2024-12-18 22:19:06 +0500

    feat: added translations for about page

--- src/pages/BookTable/BookTable.tsx
+++ src/pages/BookTable/BookTable.tsx
@@ -1,73 +1,86 @@
 import "leaflet/dist/leaflet.css";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import MapComponent from "../../components/MapComponent/MapComponent";
 import SingleSelect from "../../components/SingleSelect/SingleSelect";
+import { transformNumericals } from "../../utils/transformNumericals";
 import styles from "./BookTable.module.scss";
 
 export default function BookTable() {
   const [selectedValue, setSelectedValue] = React.useState<any>();
+  const { t, i18n } = useTranslation();
+
+  console.log(t);
+
   const numberOfPeople = React.useMemo(
     () =>
       [2, 4, 8, 10, 12].map((item) => ({
-        value: String(item),
-        label: String(item),
+        value: transformNumericals(item),
+        label: transformNumericals(item),
       })),
-    []
+    [i18n.language]
   );
 
   return (
     <div className={styles.main}>
       <div className={styles.contentContainer}>
-        <h1>Book A Table</h1>
-        <p>
-          We consider all the drivers of change gives you the components you
-          need to change to create a truly happens.
-        </p>
+        <h1>{t("aboutPage.heading")}</h1>
+        <p>{t("aboutPage.description")} </p>
 
         <form className={styles.form}>
           <div className={styles.rowInputs}>
             <label htmlFor="date" className={styles.input}>
-              <p className={styles.inputLabel}>Date</p>
+              <p className={styles.inputLabel}>
+                {t("aboutPage.inputs.date.name")}
+              </p>
               <input
                 name="date"
                 className={styles.inputAria}
-                placeholder="dd/mm/yy"
+                placeholder={t("aboutPage.inputs.date.placeholder")}
                 type="date"
               />
             </label>
             <label htmlFor="time" className={styles.input}>
-              <p className={styles.inputLabel}>Time</p>
+              <p className={styles.inputLabel}>
+                {t("aboutPage.inputs.time.name")}
+              </p>
               <input
                 name="time"
                 className={styles.inputAria}
-                placeholder="14:00"
+                placeholder={transformNumericals("14:00")}
                 type="time"
               />
             </label>
           </div>
           <div className={styles.rowInputs}>
             <label htmlFor="name" className={styles.input}>
-              <p className={styles.inputLabel}>Name</p>
+              <p className={styles.inputLabel}>
+                {t("aboutPage.inputs.name.name")}
+              </p>
               <input
                 name="name"
                 className={styles.inputAria}
                 type="text"
-                placeholder="Name"
+                placeholder={t("aboutPage.inputs.name.placeholder")}
               />
             </label>
             <label htmlFor="phone" className={styles.input}>
-              <p className={styles.inputLabel}>Phone</p>
+              <p className={styles.inputLabel}>
+                {t("aboutPage.inputs.phone.name")}
+              </p>
               <input
                 name="phone"
                 className={styles.inputAria}
                 type="number"
-                placeholder="Phone"
+                placeholder={t("aboutPage.inputs.phone.placeholder")}
               />
             </label>
           </div>
 
           <label htmlFor="numberOfPeople">
-            <p className={styles.inputLabel}>Number Of People</p>
+            <p className={styles.inputLabel}>
+              {t("aboutPage.inputs.numberOfPeople.name")}
+            </p>
             <SingleSelect
               options={numberOfPeople}
               value={selectedValue}
